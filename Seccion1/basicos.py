215cd1f70557aa2da0684becd9011f5a3d0a14ff
JoseDG <josethp1@gmail.com>
2021-07-07 09:06:42 -0500

    Ultima actualziaxion

--- Seccion1/basicos.py
+++
@@ -1,7 +0,0 @@
-
-day_hours = 24
-week_days = 7
-
-week_hours = day_hours * week_days
-
-print(week_hours)
---
+++ Seccion2-Basics-StartedPython/basicos.py
@@ -0,0 +1,7 @@
+
+day_hours = 24
+week_days = 7
+
+week_hours = day_hours * week_days
+
+print(week_hours)
